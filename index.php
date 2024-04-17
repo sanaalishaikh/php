9bee610dcb7d7d70795b30170787a4abaef593c3
sanaalishaikh <162053739+sanaalishaikh@users.noreply.github.com>
2024-04-17 22:43:34 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" conten.t="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Welcome to PhP Programming</h1>
+    <?php
+
+$name = "Sana Ali";
+
+echo "Hi How are you ?" .$name ."<br>";
+echo "Hi This is Sana Ali";
+
+
+
+
+
+?>
+
+
+
+
+
+</body>
+</html>
+
+
